d679241975d1833385e78b1c9eba085324c1842e
mishkacher <73113517+mishkacher@users.noreply.github.com>
2026-07-27 23:42:06 +0700

    test: cover system power telemetry normalization

---
+++ MacVitalsTests/SystemPowerTelemetryNormalizerTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import MacVitals
+
+final class SystemPowerTelemetryNormalizerTests: XCTestCase {
+  func testConvertsMilliwattsToWatts() {
+    XCTAssertEqual(
+      SystemPowerTelemetryNormalizer.watts(fromMilliwatts: NSNumber(value: 12_685)),
+      12.685,
+      accuracy: 0.0001)
+  }
+
+  func testRejectsNegativeAndImplausibleValues() {
+    XCTAssertNil(SystemPowerTelemetryNormalizer.watts(fromMilliwatts: NSNumber(value: -1)))
+    XCTAssertNil(SystemPowerTelemetryNormalizer.watts(fromMilliwatts: NSNumber(value: 500_001)))
+  }
+
+  func testRejectsNonNumericValue() {
+    XCTAssertNil(SystemPowerTelemetryNormalizer.watts(fromMilliwatts: "12685"))
+  }
+}
