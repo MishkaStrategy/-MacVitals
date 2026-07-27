549e35757cc8828bdfbe5b72cc464edd9be76d5c
mishkacher <73113517+mishkacher@users.noreply.github.com>
2026-07-27 13:31:41 +0700

    feat: expose fan control state to preferences

--- MacVitals/App/MacVitalsApp.swift
+++ MacVitals/App/MacVitalsApp.swift
@@ -2,14 +2,15 @@ import SwiftUI
 
 @main
 struct MacVitalsApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+  @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
-    var body: some Scene {
-        Settings {
-            PreferencesView()
-                .environmentObject(appDelegate.coordinator)
-                .environmentObject(appDelegate.settings)
-                .frame(minWidth: 620, minHeight: 520)
-        }
+  var body: some Scene {
+    Settings {
+      PreferencesView()
+        .environmentObject(appDelegate.coordinator)
+        .environmentObject(appDelegate.settings)
+        .environmentObject(appDelegate.fanControl)
+        .frame(minWidth: 620, minHeight: 520)
     }
+  }
 }
